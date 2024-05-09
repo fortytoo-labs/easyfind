7dd57ff99f4ecb9f5e490cee6eff68387368e527
Sora <34794115+sorae42@users.noreply.github.com>
2024-05-10 01:13:09 +0700

    feat(config): reorder options

--- src/main/java/net/fortytoo/easyfind/easyfind/config/ConfigAgent.java
+++ src/main/java/net/fortytoo/easyfind/easyfind/config/ConfigAgent.java
@@ -3,10 +3,12 @@ package net.fortytoo.easyfind.easyfind.config;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 public class ConfigAgent extends MidnightConfig {
-    @Entry(category = "text") public static boolean showDisabledItem;
+    @Entry(category = "text") public static boolean saveHistory = true;
+    
     @Entry(category = "text") public static boolean showDescription = true;
     @Entry(category = "text") public static boolean coloredRarity = true;
-    @Entry(category = "text") public static boolean saveHistory = true;
+    
+    @Entry(category = "text") public static boolean showDisabledItem;
     
     // TBA
     public static boolean creativeHotbarAgent;
