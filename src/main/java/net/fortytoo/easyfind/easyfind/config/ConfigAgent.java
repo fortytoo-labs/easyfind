29f2b3723e6f71ad7de2f876dcc5ac38eadca012
Sora <34794115+sorae42@users.noreply.github.com>
2024-06-14 01:37:07 +0700

    feat: prevent search close after pick

--- src/main/java/net/fortytoo/easyfind/easyfind/config/ConfigAgent.java
+++ src/main/java/net/fortytoo/easyfind/easyfind/config/ConfigAgent.java
@@ -4,6 +4,7 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class ConfigAgent extends MidnightConfig {
     public enum ReplaceNeighbor { CURRENT, NEXT, PREVIOUS }
+    public enum KeepScreen { SHIFT, ALWAYS }
     
     // Cosmetics
     @Entry(category = "text") public static boolean blurredBG = true;
@@ -12,9 +13,9 @@ public class ConfigAgent extends MidnightConfig {
     
     // Behaviour
     @Entry(category = "text") public static ReplaceNeighbor replaceNeighbor = ReplaceNeighbor.CURRENT;
+    @Entry(category = "text") public static KeepScreen keepScreenOn = KeepScreen.SHIFT;
     @Entry(category = "text") public static boolean forcedReplace = false;
-    @Entry(category = "text")
-    public static boolean ignoreExisting = false;
+    @Entry(category = "text") public static boolean ignoreExisting = false;
     
     @Entry(category = "text") public static boolean saveHistory = true;
     @Entry(category = "text") public static boolean showDisabledItem;
